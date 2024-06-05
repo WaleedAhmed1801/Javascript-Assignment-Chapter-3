7292f11b9faac330c8eb89f487685fe02a301fab
Waleed Ahmed <w.ahmed1801@gmail.com>
2024-06-05 13:05:35 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var num1 = "I am 15 Years old"
+alert(num1)
+
+var num2 = "You have visited this site 14 times"
+alert(num2)
+
+var num3 = "My birth is 1990 </br> Data type is declared Variable is number "
+document.write(num3)
+
+var user = "</br></br> <b> Waleed Ahmed </b> ordered 5 <b>T shirt </b> on <b> XYZ clothing </b> store."
+document.write(user)
